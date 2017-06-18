892c0e6c854d54afd4fcfd271b2eb774ca434b0b
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-18 13:02:28 -0600

    update controlelrs

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -18,8 +18,23 @@ class PlanetsController < ApplicationController
   end
 
   def update
+    if @planet.update(planet_params)
+      render json: @planet
+    else
+      render_error(@planet)
   end
 
   def destroy
+    @planet.destroy
+    render json: { message: 'removed' }, status: :ok
   end
+
+private
+  def set_planet
+    @planet = Planet.find(params[:id])
+  end
+
+  def planet_params
+  params.require(:planet).permit(:name, :description)
+  end  
 end
