d964e09dabe47a1565862bda68473bda5d6a3c09
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-18 13:33:35 -0600

    messed with scss, controlelrs, and planets.js

--- app/assets/javascripts/planets.js
+++ app/assets/javascripts/planets.js
@@ -1,2 +1,18 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var currentPlanet = {}
+$(document).ready( function() {
+  $('.planet-item').on('click', function() {
+    currentPlanet.id = this.dataset.id
+    $.ajax({
+      url: '/planets/' + currentPlanet.id + '/characters',
+      method: 'GET'
+      dataType: 'JSON'
+    }).done( function (characters) {
+      list.empty();
+      characters.forEach( function(char) {
+          var li = '<li data-character-id="' + char.id + '">' 
+          + char.name + '-' + char.vehicle + '</li>'
+          list.append(li)
+        });
+      });
+    });
+  });
